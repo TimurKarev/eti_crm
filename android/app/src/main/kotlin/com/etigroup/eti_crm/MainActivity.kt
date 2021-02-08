665e8cf074a8bec22ef7bd2d27ec70ab7484bb40
TimurKarev <timurkarev@mail.ru>
2021-02-08 12:58:00 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/etigroup/eti_crm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.etigroup.eti_crm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:eti_crm/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
